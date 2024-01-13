275d88ebdf4a28e1e7fccdef8b7fe0afb6b02b0c
Sameh ELGazar <selgaza95@gmail.com>
2024-01-13 22:07:43 +0200

    Advanced

--- console.py
+++ console.py
@@ -19,7 +19,6 @@ class HBNBCommand(cmd.Cmd):
     """
 
     flag = 'error'
-    
 
     def __init__(self, completeKey='tab', stdin=None, stdout=None):
         """HBNBCommand Constructor"""
@@ -40,21 +39,59 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def default(self, line):
-        """"""
-        if '.' in line:
-            args = line.split('.')  
-            if len(args) == 2:
-                cls_name, cls_method = args # [User, all()]
-                if cls_name in FileStorage.models_map:
-                    methods_map = {
-                        'create': self.do_create, 'show': self.do_show, 
-                        'all': self.do_all, 'destroy': self.do_destroy,
-                        'destroyall': self.do_destroyall, 'update': self.do_update
-                    }
-                if cls_method in methods_map:
-                    methods_map[cls_method](cls_name)
-                    return
+        """Handling User.count(), User.all(),
+            User.show(id), User.destroy(id),
+            User.update(id, attr, val)
+            Arguments:
+                method: Getting the method to execute
+                obj: Getting the instance
+                beg_params: char's index beginning of the params
+                end_params: char's index ending of the params
+                all_params: The params to pass to method
 
+                =======================================
+                = ALTERNITAVE CODE THAT I LIKED:
+                = if '.' in line:
+                =     obj, rest = line.split('.', 1)
+                =     method, params = rest.split('(', 1)
+                =     params = params.rstrip(')')
+                =     params = [p.strip(' "\'') for p in params.split(',')]
+                =     all_params = [obj] + params
+                =     all_params = ' '.join(all_params)
+                =     methods = {
+                =         'all': self.do_all,
+                =         'count': self.do_count,
+                =         'show': self.do_show,
+                =         'destroy': self.do_destroy,
+                =         'update': self.do_update
+                =     }
+                =
+                =     if method in methods:
+                =        methods[method](all_params)
+                =======================================
+            """
+        if '.' in line:
+            args = line.split('.')
+            if len(args) == 2:
+                method = args[1][:args[1].find('(')]
+                obj = args[0]
+                beg_params = args[1].find('(') + 1
+                end_params = args[1].find(')')
+                all_params = args[1][beg_params:end_params].split(',')
+                all_params.insert(0, obj)
+                all_params = [x.strip(' "\'') for x in all_params]
+                all_params = ' '.join(all_params).strip(' ')
+                methods = {
+                    'all': self.do_all,
+                    'count': self.do_count,
+                    'show': self.do_show,
+                    'destroy': self.do_destroy,
+                    'update': self.do_update
+                    }
+                if method in methods.keys():
+                    methods[method](all_params)
+        else:
+            cmd.Cmd.default(self, line)
 
     def help_quit(self):
         """The quit command help"""
@@ -131,16 +168,13 @@ class HBNBCommand(cmd.Cmd):
         if not line:
             for val in self.all_objects().values():
                 objects_list.append(val.__str__())
-                print(objects_list)
         else:
             if self.check_name(line) == HBNBCommand.flag:
                 return
             for key, val in self.all_objects().items():
                 if key.split('.')[0] == line:
                     objects_list.append(val.__str__())
-                    print(objects_list)
-                else:
-                    return
+        print(objects_list)
 
     def do_update(self, line):
         """Updating the instance by adding new attribute or,
@@ -174,6 +208,20 @@ class HBNBCommand(cmd.Cmd):
             self.store_save()
             self.store_reload()
 
+    def do_count(self, line):
+        """Printing all string representation of all instances based,
+            or not on the class name"""
+        counter = 0
+        if line:
+            if self.check_name(line) == HBNBCommand.flag:
+                return
+            for key in self.all_objects().keys():
+                if key.split('.')[0] == line:
+                    counter += 1
+        elif not line:
+            return
+        print(counter)
+
     def cast_attr(self, var):
         """Editing the attr value before saving to the file.json"""
         var = var.strip('\'"')
@@ -193,7 +241,7 @@ class HBNBCommand(cmd.Cmd):
 
     def check_name(self, name):
         """Checking if the use is writing the class name wrongly"""
-        if name not in FileStorage.models_map:
+        if name not in FileStorage.models_map.keys():
             print('** class doesn\'t exist **')
             return HBNBCommand.flag
 
