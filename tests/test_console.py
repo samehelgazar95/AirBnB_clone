c442ba8116e6f5e65b86a57723e75064bae62631
Sameh ELGazar <selgaza95@gmail.com>
2024-01-14 05:51:44 +0200

    More unit tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 """Unit tests for file Base Class"""
 import unittest
+from console import HBNBCommand
 
+class TestBase(unittest.TestCase):
+    """UnitTest for Base Class Class"""
+
+    def setUp(self):
+        '''Imports module, instantiates class'''
+        pass
+
+    def tearDown(self):
+        '''Cleans up after each test_method.'''
+        pass
 
 
 if __name__ == "__main__":
