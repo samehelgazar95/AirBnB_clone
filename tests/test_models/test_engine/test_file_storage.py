333c9daa8519e7ef6f64bb120e41b272b1f4da4e
Sameh ELGazar <selgaza95@gmail.com>
2024-01-15 22:09:41 +0200

    Tests for storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import unittest
-import datetime
+import os
 import models
-from models.engine import file_storage
+from models import base_model
 from models.engine.file_storage import FileStorage
 
 
 class TestBase(unittest.TestCase):
+    """Testing instantiation and attributes"""
 
     def setUp(self):
         '''Imports module, instantiates class'''
@@ -14,7 +15,9 @@ class TestBase(unittest.TestCase):
 
     def tearDown(self):
         '''Cleans up after each test_method.'''
-        pass
+        FileStorage._FileStorage__objects = {}
+        if os.path.exists(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
 
     def test_does_module_has_doc(self):
         doc = models.engine.file_storage.__doc__
@@ -39,6 +42,32 @@ class TestBase(unittest.TestCase):
     def test_FileStorage_all(self):
         obj = FileStorage()
         self.assertEqual(dict, type(obj.all()))
+
+    def test_new(self):
+        instance = base_model.BaseModel()
+        store = FileStorage()
+        store.new(instance)
+        key = f'{instance.__class__.__name__}.{instance.id}'
+        self.assertIn(key, store._FileStorage__objects)
+
+    def test_save(self):
+        store = FileStorage()
+        instance = base_model.BaseModel()
+        store.new(instance)
+        store.save()
+        key = f'{instance.__class__.__name__}.{instance.id}'
+        with open('file.json', 'r') as f:
+            data = f.read()
+        self.assertIn(key, data)
+
+    def test_reload(self):
+        store = FileStorage()
+        instance = base_model.BaseModel()
+        store.new(instance)
+        store.save()
+        store.reload()
+        key = f'{instance.__class__.__name__}.{instance.id}'
+        self.assertIn(key, store._FileStorage__objects)
 
 
 if __name__ == "__main__":
